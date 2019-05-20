7660b8ecd84ed100bdc959db98e084a8de27014e
Nathan Hoekstra <nathanhoekstra@hotmail.com>
2019-05-20 14:11:28 +0200

    battery_frame_action now sends the correct frames to display

--- code/headers/battery_frame_action.hpp
+++ code/headers/battery_frame_action.hpp
@@ -10,6 +10,14 @@ namespace r2d2::robos {
 
         static constexpr char battery_message[10] = "Battery: ";
 
+        // Display frame types
+        frame_cursor_color_s cursor_color;
+        frame_cursor_position_s cursor_position;
+        frame_display_8x8_character_via_cursor_s display_characters;
+
+        void red_cursor(frame_cursor_color_s &frame);
+        void green_cursor(frame_cursor_color_s &frame);
+
     public:
         battery_frame_action_c(base_comm_c &comm, actions_t &actions);
 
--- code/src/battery_frame_action.cpp
+++ code/src/battery_frame_action.cpp
@@ -4,6 +4,27 @@ namespace r2d2::robos {
     battery_frame_action_c::battery_frame_action_c(base_comm_c &comm,
                                                    actions_t &actions)
         : frame_action_c(comm, frame_type::BATTERY_LEVEL, actions) {
+        // Set cursor to Green as default
+        static constexpr uint8_t cursor_id =
+            static_cast<uint8_t>(claimed_display_cursor::ROBOS_POWER_CURSOR);
+        cursor_color.cursor_id = cursor_id;
+        cursor_position.cursor_id = cursor_id;
+        display_characters.cursor_id = cursor_id;
+
+        // Set battery information to the top left corner
+        cursor_position.cursor_x = 0;
+        cursor_position.cursor_y = 0;
+    }
+
+    void battery_frame_action_c::red_cursor(frame_cursor_color_s &frame) {
+        frame.red = 255;
+        frame.green = 0;
+        frame.blue = 0;
+    }
+    void battery_frame_action_c::green_cursor(frame_cursor_color_s &frame) {
+        frame.red = 0;
+        frame.green = 255;
+        frame.blue = 0;
     }
 
     void battery_frame_action_c::process_packet(frame_s &frame) {
@@ -18,19 +39,24 @@ namespace r2d2::robos {
 
     void battery_frame_action_c::reply_to_data() {
         if (changed) {
-            frame_display_8x8_character_s frame = {0};
-            frame.x = 0;
-            frame.y = 0;
-            frame.red = 0xFF;
-            frame.green = 0xFF;
-            frame.blue = 0xFF;
+
+            // If battery percentage is under the 30 percent, make the text red
+            // Else make the text green
+            if (battery_percentage < 30) {
+                red_cursor(cursor_color);
+            } else {
+                green_cursor(cursor_color);
+            }
 
             for (unsigned int i = 0; i < 9; i++) {
-                frame.characters[i] = battery_message[i];
+                display_characters.characters[i] = battery_message[i];
             }
-            int_to_str(battery_percentage, frame.characters + 9, 10);
+            int_to_str(battery_percentage, display_characters.characters + 9,
+                       10);
 
-            comm.send(frame);
+            comm.send(cursor_color);
+            comm.send(cursor_position);
+            comm.send(display_characters);
 
             changed = false;
         }
