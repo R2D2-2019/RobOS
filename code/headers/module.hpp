47c7857ab65b415bdf86097044cc702a04910a9e
Joepieler <be.campagne@live.nl>
2019-06-28 10:27:46 +0200

    first structure how remote control will communicate with moving platform

--- code/headers/module.hpp
+++ code/headers/module.hpp
@@ -6,9 +6,11 @@
 namespace r2d2::robos {
     class module_c : public base_module_c {
     private:
-        timed_request_c battery_request;
-        timed_request_c manual_control_request;
-        timed_request_c distance_sensor_request;
+        timed_request_c manual_control_request_speed;
+        timed_request_c manual_control_request_steer;
+        int8_t speed = 0;
+        int8_t steering_angle = 0;
+        bool brake = false;
 
     public:
         /**
@@ -16,48 +18,49 @@ namespace r2d2::robos {
          */
         module_c(base_comm_c &comm)
             : base_module_c(comm),
-              battery_request(comm, frame_type::BATTERY_LEVEL),
-              manual_control_request(comm, frame_type::MOVEMENT_CONTROL),
-              distance_sensor_request(comm, frame_type::DISTANCE) {
-            comm.listen_for_frames({frame_type::BATTERY_LEVEL,
-                                    frame_type::MOVEMENT_CONTROL,
-                                    frame_type::DISTANCE});
+              manual_control_request_speed(comm,
+                                           frame_type::MANUAL_CONTROL_SLIDER),
+              manual_control_request_steer(
+                  comm, frame_type::MANUAL_CONTROL_JOYSTICK) {
+            comm.listen_for_frames({frame_type::MANUAL_CONTROL_SLIDER,
+                                    frame_type::manual_control_request_steer});
         }
 
-        void process_battery_level(frame_s frame) {
-            // Create object to read battery struct data
-            auto battery_percentage =
-                frame.as_frame_type<frame_type::BATTERY_LEVEL>().percentage;
+        void process_movement_control_speed(frame_s frame) {
+            auto data =
+                frame.as_frame_type<frame_type::MANUAL_CONTROL_SLIDER>();
 
-            hwlib::cout << "battery percentage: "
-                        << static_cast<int>(battery_percentage) << "%"
-                        << hwlib::endl;
-            // Data recieved, reset timer
-            battery_request.mark_received();
-        }
-
-        void process_distance(frame_s frame) {
-            auto distance_frame = frame.as_frame_type<frame_type::DISTANCE>();
-
-            // Data recieved, reset timer
-            distance_sensor_request.mark_received();
-        }
-        void process_movement_control(frame_s frame) {
-            auto distance_frame =
-                frame.as_frame_type<frame_type::MOVEMENT_CONTROL>();
+            // convert to moving platform
+            // slider id 0 is left peddel and slider id 1 is right peddel
+            if (data.slider_id == 0) {
+                brake = true;
+            } else {
+                brake = false;
+                speed = data.value;
+            }
+            // send frame to moving platform
 
             // Data recieved, reset timer
             manual_control_request.mark_received();
         }
 
+        void process_movement_control_steer(frame_s frame) {
+            auto data =
+                frame.as_frame_type<frame_type::MANUAL_CONTROL_JOYSTICK>();
+
+            // convert to moving platform
+            steering_angle = data.value_x;
+            // send frame to moving platform
+
+            // Data recieved, reset timer
+            manual_control_request.mark_received();
+        }
         /**
          * Let the module process data
          */
         void process() override {
             // Set out all polling requests
-            battery_request.process();
             manual_control_request.process();
-            distance_sensor_request.process();
 
             while (comm.has_data()) {
                 auto frame = comm.get_data();
@@ -69,13 +72,7 @@ namespace r2d2::robos {
 
                 // Process the frame
                 switch (frame.type) {
-                case frame_type::BATTERY_LEVEL:
-                    process_battery_level(frame);
-                    break;
-                case frame_type::DISTANCE:
-                    process_distance(frame);
-                    break;
-                case frame_type::MOVEMENT_CONTROL:
+                case frame_type::MANUAL_CONTROL_SLIDER:
                     process_movement_control(frame);
                     break;
                 default:
