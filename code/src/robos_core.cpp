48a8ccb638eafc66ed56ff03d7211bc9a702641a
Gregory Rochat <gregory.rochat@student.hu.nl>
2019-06-28 10:44:09 +0200

    Added state machine to robos_core_c

--- code/src/robos_core.cpp
+++ code/src/robos_core.cpp
@@ -5,9 +5,25 @@ namespace r2d2::robos_core{
 		//connect with server here? or in an init function?
 	};
 	
-	void robos_core_c::process(){};
+	void robos_core_c::process(){}
 	
-	int robos_core_c::run(){return 0;};
+	int robos_core_c::run(){
+		bool end = false;
+		int x = 1;
+		while(!end){
+			switch(robos_core_c::state){
+				case wait : x = robos_core_c::wait_command();
+					break;
+				case initrole : robos_core_c::init_role();
+					break;
+				case runrole : robos_core_c::run_role();
+					break;
+				case shutdown : robos_core_c::shutdown_robos();
+					end = true;
+			}
+		}
+		return 0;
+	};
 	
 	int robos_core_c::wait_command(){return 0;};
 	int robos_core_c::init_role(){return 0;};
