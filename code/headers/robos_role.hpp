7a834b642ed0a444a400b44c7e57ac96c2ae0337
MarkGasse <mark.gasse@student.hu.nl>
2019-06-28 12:05:18 +0200

    Changed class name from robos_role to robos_role_c

--- code/headers/robos_role.hpp
+++ code/headers/robos_role.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
+#include <frame_types.hpp>
 #include <stdint.h>
 #include <vector>
-#include <frame_types.hpp>
 
 namespace r2d2::robos {
-    class robos_role {
+
+    class robos_role_c {
     private:
         std::vector<frame_type> modules;
-        std::vector<frame_type> packets; 
+        std::vector<frame_type> packets;
+
     public:
-        robos_role(){};
+        robos_role_c(){};
         virtual char *get_role_name() = 0;
         virtual std::vector<frame_type> get_required_modules();
         virtual std::vector<frame_type> get_required_packets();
