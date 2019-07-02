d5d46cd3f2dffeb6ac083e7c00c36423f7f67000
MarkGasse <mark.gasse@student.hu.nl>
2019-07-02 18:39:33 +0200

    Created seperate enum hpp so both core and role can acces with out creating circular dependencies

---
+++ code/headers/robos_core.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <base_module.hpp>
+#include <esp_32.hpp>
+#include <frame_handler.hpp>
+#include <frame_types.hpp>
+#include <hwlib.hpp>
+#include <ringbuffer.hpp>
+#include <robos_role.hpp>
+#include <timed_request.hpp>
+#include <robos_enums.hpp>
+
+namespace r2d2::robos {
+
+    class robos_core_c : public base_module_c {
+    private:
+        uint_fast16_t id;
+        r2d2::module mod_list[20];
+        robos_state state;
+        robos_roles role;
+        robos_role_c *current_role;
+        r2d2::communication::esp_32_c &esp;
+        std::array<timed_request_c, frame_type::COUNT> requests = {};
+
+    public:
+        robos_core_c(base_comm_c &comm, r2d2::communication::esp_32_c &esp);
+        //std::vector<frame_type> outgoing_frame_buffer;
+        void process() override;
+        int get_identity_packets();
+        int run();
+        int wait_command();
+        int init_role();
+        int run_role();
+        int update_modules();
+        int shutdown_robos();
+    };
+} // namespace r2d2::robos
---
+++ code/src/robos_core.cpp
@@ -0,0 +1,122 @@
+#include <robos_core.hpp>
+
+namespace r2d2::robos {
+    robos_core_c::robos_core_c(base_comm_c &comm,
+                               r2d2::communication::esp_32_c &esp)
+        : base_module_c(comm), esp(esp) {
+        comm.configure(r2d2::module::NONE, {r2d2::frame_type::ALL});
+    }
+
+    void robos_core_c::process() {
+        bool end = false;
+        int error_code;
+
+        while (!end) {
+            // esp comes here
+            // Set out all polling requests
+            for (auto &request : requests) {
+                if (request.get_type() != frame_type::NONE) {
+                    request.request_packet();
+                }
+            }
+
+            // process received frames
+            while (comm.has_data()) {
+                auto frame = comm.get_data();
+                // This module doesn't handle requests
+                if (frame.request) {
+                    continue;
+                }
+
+                // process the received packet using the appropriate
+                // frame-handler
+                // handler.process(frame);
+
+                // Data recieved, reset timer
+                auto &request = requests[frame.type];
+                if (request.get_type() != frame_type::NONE) {
+                    request.mark_received();
+                }
+            }
+
+            switch (robos_core_c::state) {
+            case robos_state::WAIT:
+                error_code = robos_core_c::wait_command();
+                break;
+            case robos_state::INITROLE:
+                error_code = robos_core_c::init_role();
+                break;
+            case robos_state::RUNROLE:
+                error_code = robos_core_c::run_role();
+                break;
+            case robos_state::UPDATEMODULES:
+                error_code = robos_core_c::update_modules();
+                break;
+            case robos_state::SHUTDOWN:
+                error_code = robos_core_c::shutdown_robos();
+                end = true;
+            }
+            end = true;
+        }
+    };
+
+    int robos_core_c::wait_command() {
+        robos_core_c::state = robos_state::INITROLE;
+        // potentially choose role here
+        return 0;
+    };
+
+    int robos_core_c::init_role() {
+        switch (robos_core_c::role) {
+        case robos_roles::MANUAL_CONTROL:
+            // auto manual_control_role = robos_role_c();
+            // robos_core_c::robos role = manual_control_role;
+            break;
+        case robos_roles::MOVING_PLATFORM:
+            // auto moving_platform_role = robos_role_c();
+            // robos_core_c::robos role = moving_platform;
+            break;
+        case robos_roles::EXAMPLE_ROLE:
+            // auto moving_platform_role = robos_role_c();
+            // robos_core_c::robos role = moving_platform;
+            break;
+        }
+
+        return 0;
+    };
+
+    int robos_core_c::run_role() {
+       // const std::vector<frame_type> test_frame;
+       // robos_core_c::current_role->run(test_frame);
+        // ringbuffer_c robos_core_c::current_role->getoutgoingframes();
+        ringbuffer_c<frame_s, 32> ringbuffer;
+        while (!ringbuffer.empty()) {
+            auto frame = ringbuffer.copy_and_pop();
+            comm.send(frame);
+        }
+        return 0;
+    };
+
+    int robos_core_c::shutdown_robos() {
+        robos_core_c::current_role->cleanup();
+        return 0;
+    };
+
+    int robos_core_c::update_modules() {
+        ringbuffer_c<frame_s, 32> ringbuffer;
+        comm.request(IDENTITY);
+        hwlib::wait_ms(1000);
+        int mod_list_counter = 0;
+        while (comm.has_data()) {
+            auto frame = comm.get_data();
+            if (frame.type == IDENTITY) {
+                const auto data = frame.as_frame_type<frame_type::IDENTITY>();
+                mod_list[mod_list_counter] = data.type;
+            } else {
+            }
+            mod_list_counter += 1;
+        }
+        return 0;
+    };
+
+} // namespace r2d2::robos
