499b7fef549f1f000c1c4e54ff6e051278ac3e7d
Olivier Verwoerd <olivier.verwoerd@student.hu.nl>
2019-07-02 16:49:43 +0200

    added role back

--- code/headers/robos_core.hpp
+++ code/headers/robos_core.hpp
@@ -19,6 +19,7 @@ namespace r2d2::robos {
         uint_fast16_t id;
         r2d2::module mod_list[20];
         robos_state state;
+        robos_roles role;
         robos_role_c *current_role;
         r2d2::communication::esp_32_c &esp;
         std::array<timed_request_c, frame_type::COUNT> requests = {};
