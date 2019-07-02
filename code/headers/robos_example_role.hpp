8670f60aef767fff1ee6a9c8c6807ae596cab62d
MarkGasse <mark.gasse@student.hu.nl>
2019-07-02 15:41:42 +0200

    Added documentation, changed function types and removed some unnecessary code

--- code/headers/robos_example_role.hpp
+++ code/headers/robos_example_role.hpp
@@ -5,16 +5,44 @@
 namespace r2d2::robos {
 
     class robos_example_role_c : public robos_role_c {
-    private:
-        enum robos_roles { MANUAL_CONTROL, MOVING_PLATFORM, EXAMPLE_ROLE };
-
     public:
+
+        /**
+         * @brief
+         * Constructor robos_example_role_c
+         *
+         * The default constructor is empty.
+         */
         robos_example_role_c();
 
-        robos_core::robos_roles get_role_name() override;
+        /**
+         * @brief
+         * This function returns the name of the role.
+         * @detail
+         * This must be overridden by the child class
+         * of the abstract class robos_role_c.
+         */
+        robos_roles get_role_name() override;
 
+        /**
+         * @brief
+         * This function handles the tasks of the role.
+         *
+         * @param std::array<frame_type, 10>: frames
+         * The required frames to process this role.
+         * @detail
+         * This function will receive data of certain modules and
+         * send the data to other modules if requirements are met.
+         * This function must be overridden by the child class
+         * of the abstract class robos_role_c.
+         */
         uint8_t run(const std::array<frame_type, 10> &frames) override;
 
+        /**
+         * @brief
+         * This function resets modules and unnecessary objects
+         * to make robOS ready for a new role.
+         */
         uint8_t cleanup() override;
     };
 } // namespace r2d2::robos
